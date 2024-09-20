96fca90643dac23d9856fd921493e05100855758
vivek722 <vivekthakkary007@gmail.com>
2024-09-20 19:04:38 +0530

    20-9-24

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { EventHandLing } from './Component/EventHandling.jsx'
+// import App from './App.jsx'
+// import { EventHandLing } from './Component/EventHandling.jsx'
+//  import { EventPropagation } from './Component/EventPropagation.jsx'
+import {Incriment} from './Component/incriementState'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <section className='container'>
+    {/* <section className='container'>
         <h1 className='natflix-heading'>best Netflix Series</h1>
    <App />
    </section>
-   <EventHandLing/>
-  </StrictMode>,
+   <EventHandLing/> */}
+   {/* <EventPropagation/> */}
+   <Incriment/>  </StrictMode>,
 )
